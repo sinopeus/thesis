16f851970435ce5be79ec294342fc35bc2c5fb5b
Xavier Goás Aguililla <xavier.goas@gmail.com>
2013-05-17 13:29:44 +0200

    Changed structure.

--- articles/scripts/crunch.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -w
-$ENV{MORPHLIB}='/home/pj/Perseus/morph/stemlib';
-# exec "../morph/bin/cruncher";
-# exec "../morph/bin/cruncher test";
-exec "../../Perseus/morph/bin/cruncher -L";
-# exec "../../Perseus/morph/bin/cruncher";
